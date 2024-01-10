2076e2d08c6b9e727c46d6557d03fb0972d55e3a
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-01-10 15:56:27 +0100

    COME

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,13 +1,6 @@
-#!/bin/bash
+--- LIST OF TABLES IN A DATABASE
+USE hbtn_0c_0;
 
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <database_name>"
-  exit 1
-fi
+SHOW TABLES;
 
-# Assign the first argument to the variable dbname
-dbname="$1"
 
-# Run the mysql command to show tables in the specified database
-mysql -e "USE $dbname; SHOW TABLES;"
