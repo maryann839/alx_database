9b94022f61e4d0198655d4b08e40f1a7ae11d5f1
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-01-10 15:52:27 +0100

    trea

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,9 +1,13 @@
---- LIST OF TABLES IN A DATABASE
---CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-mysql -u user -p
+#!/bin/bash
 
-USE hbtn_0c_0;
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <database_name>"
+  exit 1
+fi
 
-SHOW TABLES;
+# Assign the first argument to the variable dbname
+dbname="$1"
 
-
+# Run the mysql command to show tables in the specified database
+mysql -e "USE $dbname; SHOW TABLES;"
