442881d6e9015a20fb4b7ddacb12977055ecbaef
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-01-11 12:13:42 +0100

    cond

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- this scripts displays the addition of keys and values to a row 
-INSERT INTO first_table (id, name) VALUES(89, 'Hoiberton School');
+INSERT INTO first_table (id, name) VALUES(89, 'Holberton School');
 
--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
--- A Script that displays the number of records 
-SELECT COUNT(*);
-
-
+-- A Script that displays the number of records with id = 89 in the table first_table.
+SELECT COUNT(*)
+FROM first_table
+WHERE id = 89;
